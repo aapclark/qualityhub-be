db3f9e83247965f70f9bfaf6d5faf3e79667ab04
Aaron Clark <aaronpclark92@gmail.com>
2020-03-15 11:08:36 -0500

    feat: refactors for replacing gql-yoga with apollo-server

--- apollo/src/index.js
+++ apollo/src/index.js
@@ -1,13 +1,19 @@
 // require('dotenv').config()
-const express = require('express');
+// const express = require('express');
 const server = require('./server');
 
-server.express.use('/doc', express.static("doc/schema"));
+// server.express.use('/doc', express.static("doc/schema"));
+
+
 
 const options = { port: (process.env.APOLLO_PORT || process.env.PORT) || 4444 };
 
 
 
-server.start(options, ({ port }) => {
-	console.log(`QualityHub Core running on ${port}`);
+server.listen(options).then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
 });
+
+// server.start(options, ({ port }) => {
+// 	console.log(`QualityHub Core running on ${port}`);
+// });
