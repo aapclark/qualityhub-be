1a5a193e96148012be894549d0084f689e805d41
Aaron Clark <aaronpclark92@gmail.com>
2020-03-31 10:40:17 -0500

    feat: adds index for resolvers and simple import in server

---
+++ apollo/src/resolvers/index.js
@@ -0,0 +1,21 @@
+const Booking = require('./Booking')
+const Mutation = require('./Mutation')
+const Query = require('./Query')
+const Response = require('./Response')
+const ResumeReview = require('./ResumeReview')
+const Review = require('./Review')
+const User = require('./User')
+
+
+const resolvers = {
+  Booking,
+  Mutation,
+  Query,
+  Response,
+  ResumeReview,
+  Review,
+  User
+}
+
+
+module.exports = resolvers
--- apollo/src/server.js
+++ apollo/src/server.js
@@ -1,24 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 const { prisma } = require('./generated/prisma-client');
 const { buildFederatedSchema } = require('@apollo/federation');
-
-const typeDefs = require('./schema');
-const Mutation = require('./resolvers/Mutation');
-const Query = require('./resolvers/Query');
-const User = require('./resolvers/User');
-const Review = require('./resolvers/Review');
-const Response = require('./resolvers/Response');
-const ResumeReview = require('./resolvers/ResumeReview')
-
-const resolvers = {
-  Query,
-  Mutation,
-  User,
-  Review,
-  Response,
-  ResumeReview,
-};
-
+const resolvers = require('./resolvers')
 
 
 const server = new ApolloServer({
