b78e44bb08fcb63b1f66ea6f40846ae73844394d
Aaron Clark <aaronpclark92@gmail.com>
2020-03-31 18:57:43 -0500

    feat: checkFields throws field-specific error

--- apollo/src/utils.js
+++ apollo/src/utils.js
@@ -13,7 +13,7 @@ function checkFields(args) {
   console.log('checking fields on args', args)
   for (let key of Object.keys(args)) {
     if (!args[key]) {
-      throw new Error('Invalid input for required fields.');
+      throw new Error(`Invalid input for required field ${key}.`);
     }
   }
 }
