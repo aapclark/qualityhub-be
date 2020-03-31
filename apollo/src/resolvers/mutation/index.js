ac2abd934138ef74deb89ebe5a6e5ecfd705a2aa
Aaron Clark <aaronpclark92@gmail.com>
2020-03-31 16:49:00 -0500

    fix: corrects imports of Stripe and User mutations

--- apollo/src/resolvers/mutation/index.js
+++ apollo/src/resolvers/mutation/index.js
@@ -1,11 +1,12 @@
-const { Stripe } = require('./Stripe')
-// const Mutation = require('./Mutation')
-const { User } = require('./User')
+const Stripe = require('./Stripe')
+const User = require('./User')
 
 //  Breaks up and imports Mutation resolvers by concern
 
 
+
 const Mutation = { ...Stripe, ...User }
 
+console.log('mutation', Mutation)
 
 module.exports = Mutation
