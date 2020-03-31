e0c4dbaed9587bc1fdcb15f63eaf783a2812e950
Aaron Clark <aaronpclark92@gmail.com>
2020-03-31 10:47:29 -0500

    chore: removes apollo-federation import

--- apollo/src/server.js
+++ apollo/src/server.js
@@ -1,11 +1,8 @@
 const { ApolloServer } = require('apollo-server')
 const { prisma } = require('./generated/prisma-client');
-const { buildFederatedSchema } = require('@apollo/federation');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers')
-
-
 
 
 const server = new ApolloServer({
